b9225975249223f1339a0456e455ec7fdd2a83f2
6210110081 <6210110081@psu.ac.th>
2021-07-22 00:17:47 +0700

    submission

--- components/ForeCast.js
+++ components/ForeCast.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+
 export default function Forecast(props) {
+
     return (
         <View style={styles.underTextWeather}>
             <View style={styles.viewCelsius}>
